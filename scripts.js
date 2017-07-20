1418d3d5b0906f58b4c109cb2f0221a2c61e813b
kuba-rosiek <rosiek.kuba@gmail.com>
2017-07-20 16:57:00 +0100

    Update scripts.js
    Nadal nie wyswietla w konsoli

--- scripts.js
+++ scripts.js
@@ -1,15 +1,11 @@
 // scripts.js
 
-function getTriangleArea(a, h) {};
-
-var a,
-    h,
-  
-if (a,h > 0) {
-    return (a*h/2)} 
-	else if (value < 0) {
-	console.log('Nieprawidłowe dane')}
-	else {
+function getTriangleArea(a, h) {
+	if (a,h > 0)
+    return (a*h/2) 
+	else if (value < 0) 
+	console.log('Nieprawidłowe dane')
+	else 
 	console.log('Nieprawidłowe dane');
+	console.log( getTriangleArea(10,6) );
 };
-console.log( getTriangleArea(10,6) )
