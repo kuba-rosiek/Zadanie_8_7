5d804103d26fea77798956085a1eff0fbc6cab9a
unknown <rosiek.kuba@gmail.com>
2017-07-20 11:54:57 +0100

    do porawki

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// scripts.js
+
+function getTriangleArea(a, h) {};
+
+var a,
+    h,
+  
+if (a,h > 0) {
+    return (a*h/2)} 
+	else if (value < 0) {
+	console.log('Nieprawidłowe dane')}
+	else {
+	console.log('Nieprawidłowe dane');
+};
+console.log( getTriangleArea(10,6) )
