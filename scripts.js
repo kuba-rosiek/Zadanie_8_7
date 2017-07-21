2e01f4aa3cfc9318db79420665b9cbf2701ec68d
kuba-rosiek <rosiek.kuba@gmail.com>
2017-07-21 18:20:28 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@
 
 function getTriangleArea(a, h) {
 	if (a,h > 0) {
-    return (a*h/2) 
+    		return (a*h/2) 
 	} else if (value < 0) { 
-	console.log('Nieprawidłowe dane')
+		console.log('Nieprawidłowe dane')
 	} else{ 
-	console.log('Nieprawidłowe dane')};
+		console.log('Nieprawidłowe dane')};
 };
-	console.log( getTriangleArea(10,6) );
+console.log( getTriangleArea(10,6) );
