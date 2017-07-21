c21a252191c621d3a80fa4f089a2951b3a195253
kuba-rosiek <rosiek.kuba@gmail.com>
2017-07-21 13:02:03 +0100

    Update scripts.js
    Dodane klamry

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 // scripts.js
 
 function getTriangleArea(a, h) {
-	if (a,h > 0)
+	if (a,h > 0) {
     return (a*h/2) 
-	else if (value < 0) 
+	} else if (value < 0) { 
 	console.log('Nieprawidłowe dane')
-	else 
-	console.log('Nieprawidłowe dane');
+	} else{ 
+	console.log('Nieprawidłowe dane')};
 	console.log( getTriangleArea(10,6) );
 };
